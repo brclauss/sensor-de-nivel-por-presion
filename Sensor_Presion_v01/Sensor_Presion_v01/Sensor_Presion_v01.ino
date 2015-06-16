355291f2136215f7b7c04d6f522ee0ff6dc98bbe
brclauss <brclauss@gmail.com>
2015-06-15 23:41:01 -0300

    Lectura de presion, calculo de altura y comparación con el valor seteado

--- Sensor_Presion_v01/Sensor_Presion_v01/Sensor_Presion_v01.ino
+++ Sensor_Presion_v01/Sensor_Presion_v01/Sensor_Presion_v01.ino
@@ -15,13 +15,15 @@ Rev   Date         Description
 ---   ----------   ---------------
 1.0   01/05/2015   Initial release.
 1.1   25/05/2015   Prueba de hardware
+1.2   15/06/2015   Lectura de presion, calculo de altura y comparación con valor seteado
 
 *************************************/
 
 // Se importan las librerías
 #include <SFE_BMP180.h>
 #include <Wire.h>
-//Se declara una instancia de la librería
+
+// Se declara una instancia de la librería
 SFE_BMP180 pressure;
 
 // Defines
@@ -32,71 +34,72 @@ SFE_BMP180 pressure;
 #define Cero  15	// Ajuste de cero
 #define Pote  A3	// Ajuste de sensibilidad
 
-//Se declaran las variables. Es necesario tomar en cuenta una presión inicial
-//esta será la presión que se tome en cuenta en el cálculo de la diferencia de altura
+// Se declaran las variables. Es necesario tomar en cuenta una presión inicial
+// esta será la presión que se tome en cuenta en el cálculo de la diferencia de altura
 double PresionBase;
-//Leeremos presión y temperatura. Calcularemos la diferencia de altura
+
+// Leeremos presión y temperatura. Calcularemos la diferencia de altura
 double Presion = 0;
 double Altura = 0;
 double Temperatura = 0;
 char status;
 
+// Variables para la aplicación
+double d_Presion_Base = 0;
+double d_Presion = 0;
+double d_Altura = 0;
+
 int Pulsador = 0;
-int  Potes = 0;
+int Potes = 0;
+
+byte b_Leer_Base = 1;
 
 void setup()
 {
-	Serial.begin(9600);
-	//Se inicia el sensor y se hace una lectura inicial
-	SensorStart();
-	pinMode(LQ0_0, OUTPUT);
+	Serial.begin(9600);				// Inicializa puerto serie
+	SensorStart();					// Se inicia el sensor y se hace una lectura inicial
+	pinMode(LQ0_0, OUTPUT);			// Inicializa salidas digitales
 	pinMode(Err, OUTPUT);
 	pinMode(Q0_0, OUTPUT);
 	pinMode(Run, OUTPUT);
-	pinMode(Cero, INPUT);
+	pinMode(Cero, INPUT);			// Inicializa entradas digitales
 }
+
 void loop()
 {
-	Pulsador = digitalRead(Cero);
-	Potes = analogRead(Pote);
-	//Se hace lectura del sensor
-	ReadSensor();
-	//Se imprimen las variables
-	Serial.println(" ////// ");
-	Serial.print("Temperatura: ");
-	Serial.print(Temperatura);
-	Serial.println(" grados C");
-	Serial.print("Presion: ");
-	Serial.print(Presion);
-	Serial.println(" milibares");
-	Serial.print("Altura relativa: ");
-	Serial.print(Altura);
-	Serial.println(" metros");
-	//delay(2000);
-	digitalWrite(LQ0_0, HIGH);   // sets the LED on
-	delay(200);                  // waits for a second
-	digitalWrite(LQ0_0, LOW);    // sets the LED off
-	delay(200);
-	digitalWrite(Err, HIGH);   // sets the LED on
-	delay(200);                  // waits for a second
-	digitalWrite(Err, LOW);    // sets the LED off
-	delay(200);
-	digitalWrite(Q0_0, HIGH);   // sets the LED on
-	delay(1000);                  // waits for a second
-	digitalWrite(Q0_0, LOW);    // sets the LED off
-	delay(1000);
-	digitalWrite(Run, HIGH);   // sets the LED on
-	delay(200);                  // waits for a second
-	digitalWrite(Run, LOW);    // sets the LED off
-	delay(200);
-	Serial.print("Pulsador: ");
-	Serial.println(Pulsador);
-	Serial.print("Pote: ");
-	Serial.println(Potes);
+	digitalWrite(Run, HIGH);						// Enciende led de RUN
+	if (b_Leer_Base)
+	{
+		ReadSensor();								// Se hace lectura del sensor
+		d_Presion_Base = Presion;
+		b_Leer_Base = 0;
+	}
+	ReadSensor();									// Se hace lectura del sensor
+	d_Presion = Presion;
+	Potes = analogRead(Pote);						// Se lee seteo de potenciometro
+	Pulsador = digitalRead(Cero);					// Se lee estado del pulsador
+	d_Altura = (d_Presion - d_Presion_Base)*10.2;	// Calculo de altura
+	if (d_Altura >= Potes)							// Compara altura seteada con altura medida
+	{
+		digitalWrite(Q0_0, HIGH);					// Activa salida
+		digitalWrite(LQ0_0, HIGH);					// Enciende led de salida Q0_0
+	}
+	else
+	{
+		digitalWrite(Q0_0, LOW);					// Desactiva salida
+		digitalWrite(LQ0_0, LOW);					// Apaga led de salida Q0_0
+	}
+	
+	
+	
+	
+	Serial.println(d_Presion);						// Transmite dato de presion medida
 }
+
+// Funciones locales
 void SensorStart()
 {
-	//Secuencia de inicio del sensor
+	// Secuencia de inicio del sensor
 	if (pressure.begin())
 	Serial.println("BMP180 init success");
 	else
@@ -104,49 +107,50 @@ void SensorStart()
 		Serial.println("BMP180 init fail (disconnected?)\n\n");
 		while (1);
 	}
-	//Se inicia la lectura de temperatura
+	// Se inicia la lectura de temperatura
 	status = pressure.startTemperature();
 	if (status != 0)
 	{
 		delay(status);
-		//Se lee una temperatura inicial
+		// Se lee una temperatura inicial
 		status = pressure.getTemperature(Temperatura);
 		if (status != 0)
 		{
-			//Se inicia la lectura de presiones
+			// Se inicia la lectura de presiones
 			status = pressure.startPressure(3);
 			if (status != 0)
 			{
 				delay(status);
-				//Se lee la presión inicial incidente sobre el sensor en la primera ejecución
+				// Se lee la presión inicial incidente sobre el sensor en la primera ejecución
 				status = pressure.getPressure(PresionBase, Temperatura);
 			}
 		}
 	}
 }
+
 void ReadSensor()
 {
-	//En este método se hacen las lecturas de presión y temperatura y se calcula la altura
-	//Se inicia la lectura de temperatura
+	// En este método se hacen las lecturas de presión y temperatura y se calcula la altura
+	// Se inicia la lectura de temperatura
 	status = pressure.startTemperature();
 	if (status != 0)
 	{
 		delay(status);
-		//Se realiza la lectura de temperatura
+		// Se realiza la lectura de temperatura
 		status = pressure.getTemperature(Temperatura);
 		if (status != 0)
 		{
-			//Se inicia la lectura de presión
+			// Se inicia la lectura de presión
 			status = pressure.startPressure(3);
 			if (status != 0)
 			{
 				delay(status);
-				//Se lleva a cabo la lectura de presión,
-				//considerando la temperatura que afecta el desempeño del sensor
+				// Se lleva a cabo la lectura de presión,
+				// considerando la temperatura que afecta el desempeño del sensor
 				status = pressure.getPressure(Presion, Temperatura);
 				if (status != 0)
 				{
-					//Se hace el cálculo de la altura en base a la presión leída en el Setup
+					// Se hace el cálculo de la altura en base a la presión leída en el Setup
 					Altura = pressure.altitude(Presion, PresionBase);
 				}
 				else Serial.println("error en la lectura de presion\n");
